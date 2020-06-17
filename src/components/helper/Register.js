9b6a723d351e061f5c525d042f3fdaa83a0d9051
Mannie <carlos.alvarezberrios@gmail.com>
2020-06-17 01:17:44 -0400

    installed redux, created reducers and initial state, login and register post to reqres.in api successful

--- src/components/helper/Register.js
+++ src/components/helper/Register.js
@@ -1,26 +1,53 @@
 import React from 'react';
+import useForm from "../../hooks/useForm";
+import axiosWithAuth from '../../utils/axiosWithAuth';
 
 const Register = () => {
+    const [helper, handleChanges] = useForm({
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: "",
+    });
+
+    const postSignup = e => {
+        e.preventDefault();
+
+        const newHelper = {
+            //name: helper.firstName + " " + helper.lastName,
+            email: helper.email,
+            password: helper.password
+        }
+        if(newHelper.email.length > 0) {
+            axiosWithAuth()
+            .post("/register", newHelper)
+            .then(res => console.log(res.data))
+            .catch(err => console.log(err.message, err.response));
+        }
+        
+    }
+
+
     return (
         <div className = "helper-login-container">
             <h2>Helper Sign Up</h2>
             <p>Create an account and start helping.</p>
             <p>Not a helper? <a href = "#">Click Here</a>.</p>
 
-            <form>
+            <form onSubmit = {postSignup}>
                 <label htmlFor = "firstName">First Name:</label>
-                <input id = "firstName" type = "text" name = "firstName" placeholder = "First Name" />
+                <input id = "firstName" type = "text" name = "firstName" value = {helper.firstName} onChange = {handleChanges} placeholder = "First Name" />
 
                 <label htmlFor = "lastName">Last Name:</label>
-                <input id = "lastName" type = "text" name = "lastName" placeholder = "Last Name" />
+                <input id = "lastName" type = "text" name = "lastName" value = {helper.lastName} onChange = {handleChanges} placeholder = "Last Name" />
 
                 <label htmlFor = "email">Email Address</label>
-                <input id = "email" type = "email" name = "email" placeholder = "Email Address" />
+                <input id = "email" type = "email" name = "email" value = {helper.email} onChange = {handleChanges} placeholder = "Email Address" />
 
                 <label htmlFor = "password">Password</label>
-                <input id = "password" type = "password" name = "password" placeholder = "Password" />
+                <input id = "password" type = "password" name = "password" value = {helper.password} onChange = {handleChanges} placeholder = "Password" />
 
-                <button>Create Account</button>
+                <button type = "submit">Create Account</button>
 
                 {/* 
                     //Stretch to add Slack login
--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from "react-router-dom";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import reducers from "./reducers";
+import thunk from "redux-thunk";
 import './index.css';
 import App from './App';
+
+const store = createStore(reducers, applyMiddleware(thunk));
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <Router>
-    <App />
+    <Provider store = {store}>
+      <App />
+    </Provider>
   </Router>,
   rootElement
 );
