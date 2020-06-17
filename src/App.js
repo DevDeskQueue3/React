abfcb5c653ad85ba40b4064e08c452888bd406e2
Carlos Emmanuel Alvarez Berrios <carlos.alvarezberrio@gmail.com>
2020-06-17 01:57:10 -0400

    Revert "AxiosWithAuth, Redux, Helper Login"

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React from 'react';
-import { Route } from "react-router-dom";
-import HelperLogin from "./components/helper/Login";
-import HelperRegister from "./components/helper/Register";
+import logo from './logo.svg';
+import './App.css';
 
-const App = () => {
+function App() {
   return (
-    <div className = "App">
-      <Route path = "/helper/login" component = {HelperLogin} />
-      <Route path = "/helper/signup" component = {HelperRegister} />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
-};
+}
 
 export default App;
--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import { helperLogin } from "./helperLogin";
-import { studentLogin } from "./studentLogin"
-
-export default combineReducers({
-    helperLogin,
-    studentLogin,
-});
