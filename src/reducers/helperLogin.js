ec6009a583f7778216b69b01920d06fd8ae1a6e5
Carlos Emmanuel Alvarez Berrios <carlos.alvarezberrio@gmail.com>
2020-06-17 02:10:26 -0400

    Revert "Revert "AxiosWithAuth, Redux, Helper Login""

---
+++ src/reducers/helperLogin.js
@@ -0,0 +1,17 @@
+const initialState = {
+    helper: {
+        id: 23984,
+        name: "Mannie Alvarez",
+        email: "carlos.alvarezberrio@gmail.com",
+        password: "1234",
+    },
+    isFetching: false,
+    error: ""
+}
+
+export const helperLogin = (state = initialState, action) => {
+    switch(action.type) {
+        default: 
+            return state;
+    }
+}
---
+++ src/reducers/studentLogin.js
@@ -0,0 +1,12 @@
+const initialState = {
+    student: {},
+    isFetching: false,
+    error: ""
+}
+
+export const studentLogin = (state = initialState, action) => {
+    switch(action.type) {
+        default: 
+            return state;
+    }
+}
