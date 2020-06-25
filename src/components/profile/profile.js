825935050f4ae468830871ca04d47a7a90aa7b26
Darren Tebo <darrentebo83@outlook.com>
2020-06-25 00:09:50 -0400

    added profile values

--- src/components/profile/profile.js
+++ src/components/profile/profile.js
@@ -1,14 +1,34 @@
 import React from 'react';
-
+import * as MUI from '../../MaterialUI/index';
 import { useSelector } from 'react-redux';
 
+import './profile.css';
+
 const Profile = (props) => {
+    const classes = MUI.useStyles();
     const { user } = useSelector(state => state.login);
 
     console.log(user);
 
     return (
-        <h1>Profile Coming Soon...</h1>
+        <div className='profile-wrapper'>
+            <MUI.Paper
+                className={classes.profilePaper}
+                elevation={2}
+            >
+                <MUI.Typography
+                    variant='h3'
+                    component='h3'
+                >{`Welcome, ${user.name}`}</MUI.Typography>
+                <section className='user-data'>
+                    <MUI.Typography
+                        variant='h4'
+                        component='h4'>Roles: {user.roles.map(role => {
+                            return (<p key={user.id}>{role}</p>)
+                        })}</MUI.Typography>
+                </section>
+            </MUI.Paper>
+        </div>
     );
 };
 
